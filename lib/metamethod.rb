fe99b11e7d57a71d57b81323c69beb86e7a77185
Robert Gleeson <rob@flowof.info>
2012-09-26 01:24:04 +0200

    Add API documention.

--- lib/metamethod.rb
+++ lib/metamethod.rb
@@ -1,14 +1,25 @@
 require_relative "metamethod/version"
-
 module MetaMethod
+  #
+  # @return [Boolean]
+  #   Returns true if self encapsulates a public method.
+  #
   def public?
     receiver.public_methods.include? name
   end
 
+  #
+  # @return [Boolean]
+  #   Returns true if self encapsulates a protected method.
+  #
   def protected?
     receiver.protected_methods.include? name
   end
 
+  #
+  # @return [Boolean]
+  #   Returns true if self encapsulates a private method.
+  #
   def private?
     receiver.private_methods.include? name
   end
